707f3df01b7b67d576fb33ac346337e6ea559564
orsoli <fljcoli@gmail.com>
2024-08-19 14:38:08 +0200

    modification coments

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 /*
-    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-    Stabilire il vincitore, in base a chi fa il punteggio più alto.
-    Stampiamo in pagina i due tiri e il risultato
+ Dice Game
+    Generate a random number from 1 to 6 for both the player and the computer.
+    Determine the winner based on who gets the highest score.
+    Print both rolls and the result on the page.
 */
 
 // ---  Preparation phase 
